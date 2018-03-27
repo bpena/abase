4aa1d6908e01592bc32292c343b3b501ab1c2970
bpena <bernardo.pena.ramos@gmail.com>
2018-03-27 15:09:51 -0400

    added @angular/material snackBar

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { CommonModule } from '@angular/common';
 import { ConnectionService } from '@core/services/connection.service';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   imports: [
     CommonModule,
-    HttpModule
+    HttpModule,
+    MatSnackBarModule
   ],
   declarations: [
 
