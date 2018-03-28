25a701517f95a7b81c95f46fbf8b83b7b29d67d4
bpena <bernardo.pena.ramos@gmail.com>
2018-03-28 15:18:29 -0400

    build signin form

--- src/app/modules/security/security.module.ts
+++ src/app/modules/security/security.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 import { SigninComponent } from './views/signin/signin.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule
   ],
-  declarations: [SigninComponent]
+  declarations: [SigninComponent],
+  exports: [SigninComponent]
 })
 export class SecurityModule { }
