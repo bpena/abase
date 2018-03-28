4def4b2d8ffcb75c0ea9987b774256c252ece50e
bpena <bernardo.pena.ramos@gmail.com>
2018-03-28 13:02:43 -0400

    added signin view

--- src/app/modules/security/security.module.ts
+++ src/app/modules/security/security.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SigninComponent } from './views/signin/signin.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [SigninComponent]
 })
 export class SecurityModule { }
