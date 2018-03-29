7a4773676a2b9ac2feaaebe40a430c29c461af2e
bpena <bernardo.pena.ramos@gmail.com>
2018-03-29 09:41:46 -0400

    using auth service to sign in (use mock)

--- src/app/modules/security/services/auth.service.ts
+++ src/app/modules/security/services/auth.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { RequestOptionsArgs, Response, ResponseOptions } from '@angular/http';
+import { ConnectionService } from '@core/services/connection.service';
 
 @Injectable()
 export class AuthService {
 
-  constructor() { }
+  constructor(private connectionService: ConnectionService) { }
+
+  mock: Observable<Response> = Observable.create(observer => {
+    console.log('prueba');
+    const response: Response = new Response({} as ResponseOptions);
+    response.ok = false;
+    response.status = 404;
+    response.statusText = 'todo OK';
+
+    console.log(response);
+    observer.next(response);
+  });
+
+  signin(): Observable<Response> {
+    return this.mock;
+  }
+
+  signup() {
+
+  }
+
+  logout() {
+    
+  }
 
 }
