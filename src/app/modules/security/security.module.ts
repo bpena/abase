9696586067e35b84d1636bcf07d462fc6d143acb
bpena <bernardo.pena.ramos@gmail.com>
2018-03-28 20:51:13 -0400

    refactor signini component route, now use @security alias

--- src/app/modules/security/security.module.ts
+++ src/app/modules/security/security.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule } from '@angular/material';
 import { FormsModule } from '@angular/forms';
-import { SigninComponent } from './views/signin/signin.component';
+import { SigninComponent } from '@security/views/signin/signin.component';
 
 @NgModule({
   imports: [
