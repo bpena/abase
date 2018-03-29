07602593b29d325b96f2fb9ec0107fc4e6f22650
bpena <bernardo.pena.ramos@gmail.com>
2018-03-28 23:47:16 -0400

    added reactive forms and validations into signin form

--- src/app/modules/security/views/signin/signin.component.ts
+++ src/app/modules/security/views/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
+  signinForm: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.signinForm = new FormGroup({
+      username: new FormControl(null, Validators.required),
+      password: new FormControl(null, Validators.required)
+    })
   }
 
 }
