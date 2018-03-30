495abc5d75518e75f472797eff77220b5489bccb
bpena <bernardo.pena.ramos@gmail.com>
2018-03-29 23:18:35 -0400

    allow XSS and others headers

--- server/app.js
+++ server/app.js
@@ -3,6 +3,15 @@ import { user } from './routes'
 
 const app = express()
 
+if (process.env.NODE_ENV === 'development') {
+    app.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept, Token')
+        res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE OPTIONS')
+        next()
+    })
+}
+
 app.use('/api/v1/user', user)
 
 export default app
