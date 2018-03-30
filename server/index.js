7285a9d57530b9994d9256efb88bafcdbb2a2821
bpena <bernardo.pena.ramos@gmail.com>
2018-03-29 21:22:05 -0400

    added debug module to nodejs

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 import http from 'http'
+import Debug from 'debug'
 
 const PORT = 3000
+const debug = new Debug('server-fcs::root')
 
 const app = http.createServer((req, res) => {
+    debug('New request')
     res.writeHead(200, { 'Content-Type': 'text/plain' })
     res.write('Hola desde fcs')
     res.end()
 })
 
 app.listen(PORT, () => {
-    console.log(`Server running at port ${PORT}`)
+    debug(`Server running at port ${PORT}`)
 })
