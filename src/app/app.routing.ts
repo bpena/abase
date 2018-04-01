a4fb9039f94965a621026afa790a59647ebe2fa8
bpena <bernardo.pena.ramos@gmail.com>
2018-03-31 23:48:54 -0400

    added default route, que navigate to don't exist route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,8 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { SigninComponent } from '@security/views/signin/signin.component';
 
 const ROUTES: Routes = [
-    { path: '', redirectTo: 'main', pathMatch: 'full' },
-    { path: 'main', loadChildren: './modules/main/main.module#MainModule' }
+    { path: '', redirectTo: 'main/bus/list', pathMatch: 'full' },
+    { path: 'main', loadChildren: './modules/main/main.module#MainModule' },
+    { path: '**', redirectTo: 'main/bus/list' }
 ];
 
 export const AppRouting = RouterModule.forRoot(ROUTES);
