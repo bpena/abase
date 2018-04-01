2e50b69f3ee7d9823308a511065449c7b01da9e3
bpena <bernardo.pena.ramos@gmail.com>
2018-03-31 23:49:45 -0400

    added route for edit bus

--- src/app/modules/main/modules/bus/bus.module.ts
+++ src/app/modules/main/modules/bus/bus.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BusComponent } from './bus.component';
-import { BusRouting } from './bus.routing';
-import { BusListComponent } from './views/bus-list/bus-list.component';
+import { BusListComponent } from '@bus/views/bus-list/bus-list.component';
+import { BusRouting } from '@bus/bus.routing';
+import { BusComponent } from '@bus/bus.component';
+import { MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule } from '@angular/material';
+import { BusEditComponent } from '@bus/views/bus-edit/bus-edit.component';
+import { BusService } from '@bus/services/bus.service';
+import { CoreModule } from '@core/core.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     BusRouting,
-    CommonModule
+    CommonModule,
+    CoreModule,
+    FormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    ReactiveFormsModule,
   ],
-  declarations: [BusComponent, BusListComponent]
+  declarations: [ 
+    BusComponent,
+    BusListComponent,
+    BusEditComponent
+  ],
+  providers: [
+    BusService
+  ]
 })
 export class BusModule { }
--- src/app/modules/main/modules/bus/bus.routing.ts
+++ src/app/modules/main/modules/bus/bus.routing.ts
@@ -1,13 +1,16 @@
 import { Routes, RouterModule } from '@angular/router';
-import { BusComponent } from './bus.component';
 import { BusListComponent } from './views/bus-list/bus-list.component';
+import { BusEditComponent } from './views/bus-edit/bus-edit.component';
+import { BusComponent } from './bus.component';
 
 const ROUTES: Routes = [
     { 
         path: '',
         component: BusComponent,
         children: [
-            { path: 'list', component: BusListComponent }
+            { path: 'list', component: BusListComponent },
+            { path: 'new', component: BusEditComponent },
+            { path: 'edit/:id', component: BusEditComponent }
         ]
     }
 ];
