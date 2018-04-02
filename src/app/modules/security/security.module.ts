927bc4c5fa37ad1e13b34708f203b9c6b01ce113
bpena <bernardo.pena.ramos@gmail.com>
2018-04-01 20:24:53 -0400

    added user list component

--- src/app/modules/security/security.module.ts
+++ src/app/modules/security/security.module.ts
@@ -7,7 +7,9 @@ import { SecurityRouting } from '@security/security.routing';
 import { SigninComponent } from '@security/views/signin/signin.component';
 import { SecurityComponent } from '@security/security.component';
 import { AuthService } from '@security/services/auth.service';
-import { UserMenuComponent } from './views/user-menu/user-menu.component';
+import { UserMenuComponent } from '@security/views/user-menu/user-menu.component';
+import { UserListComponent } from '@security/views/user-list/user-list.component';
+import { UserService } from '@security/services/user.service';
 
 @NgModule({
   imports: [
@@ -26,13 +28,15 @@ import { UserMenuComponent } from './views/user-menu/user-menu.component';
     SigninComponent,
     SecurityComponent,
     SignupComponent,
-    UserMenuComponent
+    UserMenuComponent,
+    UserListComponent
   ],
   exports: [
     UserMenuComponent
   ],
   providers: [
-    AuthService
+    AuthService,
+    UserService
   ]
 })
 export class SecurityModule { }
