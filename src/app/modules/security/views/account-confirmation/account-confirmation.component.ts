3f47bb5da58030026bed5046f492eafa2f52647d
bpena <bernardo.pena.ramos@gmail.com>
2018-04-09 13:05:34 -0400

    account activation view

--- src/app/modules/security/views/account-confirmation/account-confirmation.component.ts
+++ src/app/modules/security/views/account-confirmation/account-confirmation.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@security/services/auth.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { User } from '@security/model/user';
 
 @Component({
   selector: 'app-account-confirmation',
@@ -8,13 +9,24 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./account-confirmation.component.scss']
 })
 export class AccountConfirmationComponent implements OnInit {
+  private hasError = false;
+  private user: User;
+
   constructor(private activatedRouter: ActivatedRoute,
             private authService: AuthService,
             private router: Router) { }
 
   ngOnInit() {
     const activationHash = this.activatedRouter.snapshot.params.activationHash || 'empty';
-    this.authService.activateAccount(activationHash).subscribe(value => console.log(value))
+    this.authService.activateAccount(activationHash)
+      .subscribe(
+        value => {
+          this.hasError = false;
+          this.user = value;
+          console.log(value);
+        },
+        error => this.hasError = true
+      )
     console.log(activationHash);
   }
 
